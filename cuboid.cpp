81397777df674db4e606256e2a7969a9286a2674
Rahul90411 <rk3633972@gmail.com>
2025-09-21 16:09:35 +0530

    Add files via upload

---
+++ cuboid.cpp
@@ -0,0 +1,18 @@
+//	Write a program to calculate volume of a cuboid. 
+#include<iostream>
+using namespace std;
+int main()
+{
+	float l,w,h;
+	float v;
+	cout<<"enter length=";
+	cin>>l;
+		cout<<"enter width =";
+	cin>>w;
+		cout<<"enter height =";
+	cin>>h;
+	v=l*w*h;
+	cout<<"volume of cuboid="<<v;
+	return 0;
+	
+}
