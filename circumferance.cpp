5f10a85ca1420927efa33f8cbab12462397dbd01
Rahul90411 <rk3633972@gmail.com>
2025-09-21 16:04:36 +0530

    Add files via upload

---
+++ circumferance.cpp
@@ -0,0 +1,13 @@
+//	Write a program to calculate circumference of a circle. 
+#include<iostream>
+using namespace std;
+int main()
+{
+	float pi=3.14,r,ccr;
+	cout<<"enter the value of r=";
+	cin>>r;
+	ccr=2*pi*r;
+	cout<<"the value of circumferance of a circle="<<ccr;
+	return 0;
+	
+}
